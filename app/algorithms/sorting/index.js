a0f2f20b8da4f8c24e96fd58885c061447fb6864
gorritocodes <gorritocodes@gmail.com>
2025-06-23 15:42:59 -0400

    setup base file structure

---
+++ app/algorithms/sorting/index.js
@@ -0,0 +1,15 @@
+import { bubbleSort } from "./bubbleSort.js";
+import { selectionSort } from "./selectionSort.js";
+import { insertionSort } from "./insertionSort.js";
+import { mergeSort } from "./mergeSort.js";
+import { quickSort } from "./quickSort.js";
+import { heapSort } from "./heapSort.js";
+
+export {
+    bubbleSort,
+    selectionSort,
+    insertionSort,
+    mergeSort,
+    quickSort,
+    heapSort,
+};
---
+++ app/algorithms/sorting/insertionSort.js
@@ -0,0 +1,3 @@
+export default function insertionSort(arr) {
+    return arr;
+} //InsertionSort
